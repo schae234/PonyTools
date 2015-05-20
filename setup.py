e52956527cca46ccf32137df1c7789bc6b8e8ab0
Rob S <schae234@gmail.com>
2015-05-20 12:44:21 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages, Extension
 
 setup(
     name = 'ponytools',
-    version = '0.1.2',
+    version = '0.1.3',
     packages = find_packages(),
     scripts = [
        'bin/pyDi'     
