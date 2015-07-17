c746f5f177c39f35656c38dede6c214aff05839d
Rob <schae234@umn.edu>
2015-07-17 05:43:49 -0500

    updated imports for module level

--- ponytools/__init__.py
+++ ponytools/__init__.py
@@ -1,7 +1,14 @@
 
+__version__ = '0.1.2'
+
+__license__ = ('Creative Commons Attribution-NonCommercial 4.0 International License '
+                  'http://creativecommons.org/licenses/by-nc/4.0/')
+
 from .VCF import VCF
 from .Allele import Allele
 from .AxiomGeno import AxiomGeno
+from .AxiomAnnot import AxiomAnnot
+from .AxiomCalls import AxiomCalls
 from .Fasta import Fasta
 from .Trio import Trio
 from .Variant import Variant
