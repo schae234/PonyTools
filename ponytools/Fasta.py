5c043c4029fa574ecdec260fc0ed6c26fd53c983
Rob <schae234@umn.edu>
2015-07-17 05:33:05 -0500

    Made mentions to Chromosome more apparent and raises unknown chromosome exception

--- ponytools/Fasta.py
+++ ponytools/Fasta.py
@@ -1,25 +1,34 @@
 from collections import defaultdict
-from ponytools.Tools import chromosome, log
+from ponytools.Tools import Chromosome, log
+
+from .Exceptions import MissingChromosomeError
 
 class Fasta(object):
     def __init__(self):
+        self._file = None
         self.added_order = []
         self.chroms = {}
         self.attributes = defaultdict(list)
 
-    def add_chrom(self,chrom_name,sequence):
+    def add_chrom(self,chrom_name,chromosome):
         log("Adding new chromosome: {}",chrom_name)
         self.added_order.append(chrom_name)
-        self.chroms[chrom_name] = sequence
+        self.chroms[chrom_name] = chromosome
+
     def add_attribute(self,chrom_name,attr):
         self.attributes[chrom_name].append(attr)
 
     def __getitem__(self,chrom_name):
+        if chrom_name not in self.chroms:
+            raise MissingChromosomeError(
+                '{} not in Fasta: {}'.format(chrom_name,self._file)
+            )
         return self.chroms[chrom_name]
 
     @classmethod
     def from_file(cls,fasta_file):
         fasta = cls()
+        fasta._file = fasta_file
         with open(fasta_file,'r') as IN: 
             cur_chrom = None
             cur_seqs = []
@@ -27,7 +36,7 @@ class Fasta(object):
                 line = line.strip()
                 if line.startswith('>'):
                     if cur_chrom:
-                        fasta.add_chrom(cur_chrom,chromosome("".join(cur_seqs)))
+                        fasta.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
                     name,*attrs = line.lstrip('>').split()
                     cur_chrom = name
                     cur_seqs = []
@@ -36,5 +45,8 @@ class Fasta(object):
                 else:
                     cur_seqs.append(line)
             # Add the last chromosome
-            fasta.add_chrom(cur_chrom,chromosome("".join(cur_seqs)))
+            fasta.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
         return fasta
+
+
+
