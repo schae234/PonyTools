51b0e8fd13a4b7c7b59d2dd193def56fb3aa79c1
Rob <schae234@umn.edu>
2015-07-17 05:30:08 -0500

    One last commit before we depreciate this class

--- ponytools/AxiomGeno.py
+++ ponytools/AxiomGeno.py
@@ -1,16 +1,26 @@
+import pandas as pd
+from .AxiomAnnot import AxiomAnnot
+from .Fasta import Fasta
+from .Tools import log,skipno
+
+from .Exceptions import MissingChromosomeError
+
+
 class AxiomGeno(object):
     def __init__(self):
         self.samples = pd.DataFrame()
         self.CELMap = {}
         self.genos = pd.DataFrame()
-        self.annots = annotation()
+        self.annots = AxiomAnnot()
         self.fasta = Fasta()
+
         self.genomap = {
             -1 : './.',
             0  : '0/0',
             1  : '0/1',
             2  : '1/1'
         }
+
     def add_sample_table(self,sample_table):
         log("Adding samples")
         # Get rid of spaces in column headers
@@ -19,6 +29,7 @@ class AxiomGeno(object):
         for CEL,sample in sample_table[['BestArray','SampleName']].itertuples(index=False):
             self.CELMap[CEL] = sample
         self.samples = pd.concat([self.samples,sample_table])
+
     def CEL2id(self,CEL):
         ''' returns the id for a CEL, if available, otherwise CEL'''
         if CEL in self.CELMap:
@@ -27,8 +38,10 @@ class AxiomGeno(object):
             return self.CELMap[CEL.replace('.CEL','')]
         else:
             return CEL
+
     def geno2AB(self,geno):
         return self.genomap[int(geno)]
+
     def add_design(self,table):
         ''' This method is for adding additional probes (i.e. a seperate design) 
             to an existing set of samples. It 'appends' probes to the end of the 
@@ -36,7 +49,7 @@ class AxiomGeno(object):
         table.columns  = [self.CEL2id(CEL) for CEL in table.columns] 
         new_columns = list(table.columns)
         # take care of duplicate columns:
-        for id,num_seen in [(key,len(list(group))) for key,group in groupby(sorted(new_columns))]:
+        for id,num_seen in [(key,len(list(group))) for key,group in table.groupby(sorted(new_columns))]:
             if num_seen > 1:
                 for i in range(1,num_seen+1):
                     new_columns[new_columns.index(id)] = "{}_{}".format(id,i) 
@@ -53,6 +66,7 @@ class AxiomGeno(object):
         # join the samples
         self.genos = self.genos.join(table,how='outer')
         self.genos.fillna(-1)
+
     def add_annotation(self,annot):
         ''' integrates an annotations object '''
         self.annots = self.annots + annot
@@ -107,8 +121,11 @@ class AxiomGeno(object):
             for chrom_name in self.fasta.added_order:
                 log('Printing {}',chrom_name)
                 # Retrieve the probes associated with that chromosome
-                self.annots.table[self.annots.table.cust_chr == chrom_name]
-                variants = self.annots.table[self.annots.table.cust_chr == chrom_name][['cust_chr','cust_pos','Allele A','Allele B']].join(self.genos).sort('cust_pos',ascending=True)
+                variants = self.annots.table[
+                    self.annots.table.cust_chr == chrom_name
+                ][['cust_chr','cust_pos','Allele A','Allele B']].join(
+                    self.genos
+                ).sort('cust_pos',ascending=True)
                 for id,var in variants.iterrows():
                     chrom,pos,allele_A,allele_B = var[['cust_chr','cust_pos','Allele A','Allele B']] 
                     genotypes = [str(self.genomap[x])+':42' for x in var[self.genos.columns]]
@@ -117,13 +134,15 @@ class AxiomGeno(object):
                     if self.fasta[chrom][pos].upper() == allele_A.upper():
                         ref = allele_A.upper()
                         alt = allele_B.upper()
+                        print("\t".join(list(map(str,[chrom,int(pos),id,ref,alt,'.','PASS','SNP=1','GT:GQ']))+genotypes),file=OUT)
                     elif self.fasta[chrom][pos].upper() == allele_B.upper():
                         ref = allele_B.upper()
                         alt = allele_A.upper()
                     else:
                         log("Probe alleles do not match ref for {}({}:{}): ref:{} A:{} B:{}".format(id,chrom,pos,self.fasta[chrom][pos],allele_A,allele_B))
                         continue
-                    print("\t".join(list(map(str,[chrom,int(pos),id,ref,alt,'.','PASS','SNP=1','GT:GQ']))+genotypes),file=OUT)
+
+
 
     def get_pos(self,chr,pos):
         id = self.annots.table[(self.annots.table.cust_chr == str(chr)) & (self.annots.table.cust_pos == int(pos))]["Probe Set ID"][0]
@@ -140,7 +159,7 @@ class AxiomGeno(object):
     def from_files(cls,sample_file=None,geno_files=None,fasta_file=None,annot_files=None):
         cls = cls()
         cls.add_sample_table(pd.read_table(sample_file,sep=','))
-        for file in files:
+        for file in geno_files:
             log('adding table: {}',file)
             table = pd.read_table(file,skiprows=skipno(file))
             table.index = table['probeset_id']
