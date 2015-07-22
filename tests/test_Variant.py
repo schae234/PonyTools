902d33f4c5f3389a60ac2a7297e609bf3b38f95e
Rob <schae234@umn.edu>
2015-07-22 00:58:49 -0500

    added Variant tests

--- tests/test_Variant.py
+++ tests/test_Variant.py
@@ -4,6 +4,8 @@
 
 import pytest
 import ponytools as pc
+import numpy as np
+from ponytools.Exceptions import InfoKeyError
 
 @pytest.fixture
 def smpl_var():
@@ -14,6 +16,17 @@ def smpl_var():
         info='.',fmt='GT:GQ',
         genos=['0/0:42','0/1:42','1/1:42']
     )
+
+@pytest.fixture
+def missing_var():
+    return pc.Variant(
+        'chr1',100,'SNP1',
+        'A','C',
+        qual=100,fltr='PASS',
+        info='.',fmt='GT:GQ',
+        genos=['./.:42','./.:42','1/1:42']
+    )
+
 
 def test_variant_init(smpl_var):
     var = smpl_var
@@ -75,3 +88,33 @@ def test_add_info(smpl_var):
     smpl_var.add_info('foo','bar')
     assert 'foo=bar' in smpl_var.info.split(';')
 
+
+def test_add_empty_info(smpl_var):
+    smpl_var.info = '.'
+    smpl_var.add_info('foo','bar')
+    assert smpl_var.info == 'foo=bar'
+
+
+def test_get_info(smpl_var):
+    smpl_var.add_info('foo','bar')
+    assert smpl_var.get_info('foo') == 'bar'
+
+def test_get_info_raise_InfoKeyError(smpl_var):
+    with pytest.raises(InfoKeyError):
+        smpl_var.get_info('foo')
+
+def test_alt_freq(smpl_var):
+    assert smpl_var.alt_freq() == 0.5
+
+def test_alt_freq_sample_i(smpl_var):
+    assert smpl_var.alt_freq(samples_i=[1,2]) == 0.75 # should be 0/1 and 1/1
+    assert smpl_var.alt_freq(samples_i=[2]) == 1.0
+
+def test_call_rate(missing_var):
+    assert np.isclose(missing_var.call_rate() ,1/3)
+
+def test_too_many_missing_alt_freq(missing_var):
+    assert missing_var.alt_freq() is None
+    
+def test_subtraction():
+    assert pc.Variant('chr1',1000,'SNP1','A','C') - pc.Variant('chr1',1500,'SNP2','T','G') == 500    
