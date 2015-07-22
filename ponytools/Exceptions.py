a5518860ffe3ff2cdbd042c68d1c4fa514ddcff1
Rob <schae234@umn.edu>
2015-07-22 00:53:55 -0500

    Added exception for when info keys are absent

--- ponytools/Exceptions.py
+++ ponytools/Exceptions.py
@@ -10,3 +10,8 @@ class MissingChromosomeError(PonyToolsError):
     def __init__(self,expr,message=''):
         self.expr = expr
         self.message = message
+
+class InfoKeyError(PonyToolsError):
+    def __init__(self,expr,message=''):
+        self.expr = expr
+        self.message = message
