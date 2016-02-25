d2d03254362a778256f4c1c3178ae88bc123a6ca
Rob <schae234@umn.edu>
2016-02-25 10:56:32 -0600

    remove committed debug code

--- ponytools/__init__.py
+++ ponytools/__init__.py
@@ -4,13 +4,23 @@ __version__ = '0.1.3'
 __license__ = ('Creative Commons Attribution-NonCommercial 4.0 International License '
                   'http://creativecommons.org/licenses/by-nc/4.0/')
 
+print('VCF')
 from .VCF import VCF
+print('Allele')
 from .Allele import Allele
+print('AxiomGeno')
 from .AxiomGeno import AxiomGeno
+print('AxiomAnnot')
 from .AxiomAnnot import AxiomAnnot
+print('AxiomCalls')
 from .AxiomCalls import AxiomCalls
+print('Fasta')
 from .Fasta import Fasta
+print('Trio')
 from .Trio import Trio
+print('Variant')
 from .Variant import Variant
+print('VVCFBuffer')
 from .VCFBuffer import VCFBuffer  
+print('Done')
 
