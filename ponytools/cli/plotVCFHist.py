bdacd2996a0f929984b4c78712ff9685276d60ca
Rob Schaefer <schae234@gmail.com>
2016-09-15 15:12:43 -0500

    remove old CLI args and add a LST option for vcfstats

--- ponytools/cli/plotVCFHist.py
+++ ponytools/cli/plotVCFHist.py
@@ -15,50 +15,94 @@ import logging
 
 from matplotlib import pyplot as plt
 from collections import defaultdict
+from pysam import VariantFile
 
 import ponytools as pc
+import glob
 
-def plotVCFHist(args):
-    vcf = pc.VCF(args.vcf)
-    if args.sumstat == 'maf':
-        alt_freqs = [v.alt_freq() for v in vcf.iter_variants()] 
-        alt_freqs = list(filter(None,alt_freqs))
-        plt.hist(alt_freqs,bins=args.bins)
-        plt.xlabel('Alt Allele Freq.')
-        plt.ylabel('Observations')
-        if args.title is not None:
-            plt.title('{} Alt Allele Dist. (n={})'.format(args.title,len(vcf.samples)))
-        if args.out is  None:
-            args.out = os.path.split(args.vcf)[1].replace('.vcf','_AltFreq.png')
-    elif args.sumstat == 'call_rate':
-        call_rate = [v.call_rate() for v in vcf.iter_variants()]
-        call_rate = list(filter(None,call_rate))
-        plt.hist(call_rate,bins=args.bins)
-        plt.xlabel('SNP Call Rate')
-        plt.ylabel('Frequency')
-        if args.title is not None:
-            plt.title('{} Call Rate.'.format(args.title,len(vcf.samples)))
-        if args.out is  None:
-            args.out = os.path.split(args.vcf)[1].replace('.vcf','_CallRate.png')
-    elif args.sumstat == 'inter_distance':
-        positions = defaultdict(list)
-        for var in vcf.iter_variants():
-            alt_freq = var.alt_freq()
-            if alt_freq is not None  and alt_freq > 0.01:
-                positions[var.chrom].append(var.pos) 
-        distances = []
-        for chrom,pos in positions.items():
-            sorted_pos = sorted(pos)
-            for i in range(1,len(pos)):
-                distances.append(pos[i]-pos[i-1])
- 
-        distances = list(filter(None,distances))
-        plt.xlabel('Inter SNP Distance (log10)')
-        plt.hist(np.log10(distances),bins=args.bins)
-        plt.ylabel('Frequency')
-        if args.title is not None:
-            plt.title('{} InterSNP Distance.'.format(args.title,len(vcf.samples)))
-        if args.out is  None:
-            args.out = os.path.split(args.vcf)[1].replace('.vcf','_InterDistance.png')
-    plt.savefig(args.out)
+class SampleMask(object):
+    def __init__(self, id_file, samples):
+        self.name = os.path.basename(id_file).replace('.ids','')
+        self.ids = set([line.strip() for line in open(id_file).readlines()])
+        self.mask = np.array([x in self.ids for x in samples])
+        self.mafs = []
 
+    def process_genos(self,genos):
+        # get the correct genos for the mask
+        genos = genos[self.mask] 
+        self.mafs.append(self.maf(genos))
+
+    @staticmethod
+    def maf(genos):
+        genos = genos[np.isfinite(genos)]
+        if len(genos) == 0:
+            return np.nan
+        else:
+            return sum(genos)/(2*len(genos))
+
+def record_distance(a,b):
+    if a[0] != b[0]:
+        return np.nan
+    else:
+        return abs(a[1]-b[1])
+
+def VCFStats(args):
+    vcf = VariantFile(args.vcf)
+    # get an array of the samples
+    samples = np.array(vcf.header.samples)
+
+    # Generate a set of SNPs to analyze
+    print('Reading in LST file...',end='')
+    lst = set(
+        tuple(line.strip().split('\t')) \
+        for line in open(args.lst,'r')
+    )
+    print('Done\n')
+    
+    # generate sample masks
+    sample_masks = []
+    for id_file in args.ids:
+        sample_masks.append(SampleMask(id_file,samples))
+    # Initialize some bogus shit 
+    prev_pos = ('',0)
+    inter_distance = []
+    total_maf = []
+    ids = []
+    chrom = []
+    pos = []
+    for i,record in enumerate(vcf):
+        if (record.chrom,str(record.pos)) not in lst:
+            continue
+        ids.append(record.id)
+        # Calculate MAFs
+        genos = np.array([
+            sum(x.allele_indices) \
+            if None not in x.allele_indices \
+            else np.nan \
+            for x in record.samples.values()
+        ])
+        for sample in sample_masks:
+            sample.process_genos(genos)
+        # Calculate Distance
+        cur_pos = (record.chrom,record.pos)
+        inter_distance.append(
+            record_distance(prev_pos,cur_pos)
+        )
+        chrom.append(record.chrom)
+        pos.append(record.pos)
+        prev_pos = cur_pos
+        # Calculate Total MAF
+        total_maf.append(SampleMask.maf(genos))
+        # Update log 
+        if i % 100000 == 0: 
+            print("On {}".format(i))
+    # Generate the Data Frame
+    data = pd.DataFrame({x.name:x.mafs for x in sample_masks})
+    data.insert(0,'ID',ids)
+    data.insert(1,'chr',chrom)
+    data.insert(2,'pos',pos)
+    data.insert(4,'distance',inter_distance)
+    data.insert(5,'maf',total_maf)
+    # Output Data    
+    data.to_csv(args.out+'.tsv',sep='\t')
+    return None
