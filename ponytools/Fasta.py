8421fa313f163a52470485ac7562279b75520679
Rob <schae234@umn.edu>
2017-03-16 10:58:01 -0500

    Add class for accessing Fasta files

--- ponytools/Fasta.py
+++ ponytools/Fasta.py
@@ -3,11 +3,14 @@ from ponytools.Tools import Chromosome, log
 
 from .Exceptions import MissingChromosomeError
 
+import re
+
 class Fasta(object):
     def __init__(self):
         self._file = None
         self.added_order = []
         self.chroms = {}
+        self.nicknames = {}
         self.attributes = defaultdict(list)
 
     def add_chrom(self,chrom_name,chromosome):
@@ -19,34 +22,44 @@ class Fasta(object):
         self.attributes[chrom_name].append(attr)
 
     def __getitem__(self,chrom_name):
-        if chrom_name not in self.chroms:
+        try:
+            if chrom_name in self.chroms:
+                return self.chroms[chrom_name]
+            if chrom_name in self.nicknames:
+                return self.chroms[self.nicknames[chrom_name]]
+        except Exception as e:
             raise MissingChromosomeError(
                 '{} not in Fasta: {}'.format(chrom_name,self._file)
             )
-        return self.chroms[chrom_name]
 
     @classmethod
-    def from_file(cls,fasta_file):
-        fasta = cls()
-        fasta._file = fasta_file
-        with open(fasta_file,'r') as IN: 
+    def from_file(cls,fasta_file,nickname=None):
+        self = cls()
+        self._file = fasta_file
+        with open(self._file,'r') as IN: 
             cur_chrom = None
             cur_seqs = []
             for line in IN:
                 line = line.strip()
                 if line.startswith('>'):
+                    # Finish the last chromosome before adding a new one
                     if cur_chrom:
-                        fasta.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
+                        self.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
                     name,*attrs = line.lstrip('>').split()
                     cur_chrom = name
                     cur_seqs = []
                     for attr in attrs:
-                        fasta.add_attribute(attr)
+                        self.add_attribute(cur_chrom,attr)
+                    if nickname != None:
+                        pattern,replace = nickname
+                        alt = re.sub(pattern,replace,line)
+                        log('Found a nickname: mapping {} -> {}',alt,name)
+                        self.nicknames[alt] = name
                 else:
                     cur_seqs.append(line)
             # Add the last chromosome
-            fasta.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
-        return fasta
+            self.add_chrom(cur_chrom,Chromosome("".join(cur_seqs)))
+        return self
 
 
 
