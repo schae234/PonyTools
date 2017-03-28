b6696a4c198c8d80e9f47ef9cddff3f17a0b8eaf
Rob <schae234@umn.edu>
2017-03-28 11:52:10 -0500

    update tests for VCF and Annot

--- tests/test_MNEcAnnot.py
+++ tests/test_MNEcAnnot.py
@@ -1,3 +1,7 @@
-#test that a random subset of the ref and alt alleles match the fasta file
-def test_REF():
-    assert False
+import ponytools as pc
+annot = pc.MNEc2MAnnot()
+
+def test_init():
+    assert all([x.startswith('AX') for x in annot._annot.ProbeSetID])
+    assert all([x.startswith('Affx') for x in annot._annot.AffySNPID])
+    assert all([x.startswith('MNEc') for x in annot._annot.MNEcID])
--- tests/test_VCF.py
+++ tests/test_VCF.py
@@ -9,6 +9,12 @@ def init_test(smpl_vcf):
     assert smpl_vcf.pos('chr1',100).alleles() == ['0/0','0/0','0/0','0/0','0/0']
 
 
+def test_pos_index(smpl_vcf):
+    assert smpl_vcf.pos('chr1',100).alleles() == ['0/0','0/0','0/0','0/0','0/0']
+    assert smpl_vcf.pos('chr2',2000).alleles() == ['0/0','0/0','0/0','1/0','1/0']
+    assert smpl_vcf.pos('chr3',30000).alleles() == ['0/0','0/0','1/0','1/0','1/0']
+    assert smpl_vcf.pos('chr4',400000).alleles() == ['1/1','1/1','1/1','1/1','1/1']
+
 def test_concord_vcf(smpl_vcf):
     concordance = smpl_vcf.concord_VCF(smpl_vcf)
     assert all([x[1]==1.0 for x in concordance[1:]])
