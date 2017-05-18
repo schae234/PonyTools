39975085a04c462fb977d74df533a1949fd8555a
Rob <schae234@umn.edu>
2017-05-18 16:11:55 -0500

    copy the makefule to the pwd when you run impute command

--- ponytools/cli/impute.py
+++ ponytools/cli/impute.py
@@ -1,4 +1,10 @@
-
+import pkg_resources
 
 def impute(args):
-    pass
+    makefile = open(
+        pkg_resources.resource_filename('ponytools','data/ImputationMakefile'),mode='r'
+    )
+    with open('Makefile','w') as OUT:
+        print(''.join(makefile.readlines()),file=OUT)
+
+
