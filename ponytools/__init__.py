8b04f9d3d528b30da2d32db7dfa70894f1fccf9f
Rob <schae234@umn.edu>
2017-08-16 15:32:19 -0500

    make vardab available through API

--- ponytools/__init__.py
+++ ponytools/__init__.py
@@ -24,6 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ''')
 
+from .VarDab import VarDab
 from .VCF import VCF
 from .Allele import Allele
 from .AxiomAnnot import AxiomAnnot
