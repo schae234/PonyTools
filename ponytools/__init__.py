9b373bcd76b70aebc3f7a90b321a1f462e243875
Rob <schae234@umn.edu>
2017-09-14 11:11:36 -0500

    remove extraneous imports

--- ponytools/__init__.py
+++ ponytools/__init__.py
@@ -29,7 +29,4 @@ from .Allele import Allele
 from .AxiomAnnot import AxiomAnnot
 from .MNEcAnnot import MNEc2MAnnot
 from .AxiomCalls import AxiomCalls
-from .Fasta import Fasta
-from .Trio import Trio
 from .Variant import Variant
-from .VCFBuffer import VCFBuffer  
